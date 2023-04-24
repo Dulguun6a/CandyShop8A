a96c62722edbb01becfd5f98a2121d751f848875
Dulguun6a <80145304+Dulguun6a@users.noreply.github.com>
2023-04-24 07:09:12 +0000

    Products

---
+++ purchase.js
@@ -0,0 +1,85 @@
+function PrintProducts() {
+    const productsContainer = document.getElementById("products");
+    let newDiv = "";
+  
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+  
+      console.log(`${product.name} - ${product.price}`);
+  
+      const div = document.createElement("div");
+      div.classList.add("product");
+      div.innerHTML = `
+        <h3 class="title">${product.name}</h3>
+        <p>${product.description}</p>
+        <p id="price">${product.price}</p>
+        <button onclick="AddItem('${product.name}')">add</button>
+        <button onclick="MinusItem('${product.name}')">minus</button>
+      `;
+  
+      newDiv += div.outerHTML;
+    }
+  
+    productsContainer.innerHTML = newDiv;
+  }
+  
+  // window.addEventListener("load", PrintProducts);
+  window.onload = PrintProducts;
+  
+  const userbasket = [];
+  
+  function AddItem(itemName) {
+    const itemIndex = userbasket.findIndex((item) => item.name === itemName);
+  
+    if (itemIndex !== -1) {
+      userbasket[itemIndex].quantity += 1;
+    } else {
+      userbasket.push({ name: itemName, quantity: 1 });
+    }
+  
+    console.log("add: ", userbasket);
+  }
+  
+  function MinusItem(itemName) {
+    const itemIndex = userbasket.findIndex((item) => item.name === itemName);
+  
+    if (itemIndex !== -1) {
+      userbasket[itemIndex].quantity -= 1;
+  
+      if (userbasket[itemIndex].quantity === 0) {
+        userbasket.splice(itemIndex, 1);
+      }
+    }
+  
+    console.log("minus", userbasket);
+  }
+  
+  function Purchase() {
+    const total = 0;
+    for (let i = 0; i < products.length; i++) {
+      for (let j = 0; i < userbasket.length; i++) {
+        if (userbasket[i].name === products[i].name) {
+          total += products[i].price * userbasket[j].quantity;
+        }
+      }
+    }
+    document.getElementById("total").innerHTML = total;
+  }
+  
+  function Purchase() {
+    let total = 0;
+    for (let i = 0; i < userbasket.length; i++) {
+      const { name, quantity } = userbasket[i];
+      const product = products.find((p) => p.name === name);
+      if (product) {
+        total += product.price * quantity;
+      }
+    }
+  
+    document.getElementById("total").innerHTML = total;
+    Clear();
+  }
+  
+  function Clear() {
+    userbasket = [];
+  }
